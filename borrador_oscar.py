7849379eb27c21b9eabba503fb29f9fd085d0aa8
oscarinn295 <oscarmolinas295@gmail.com>
2024-07-23 00:48:31 -0300

    Update borrador_oscar.py

--- borrador_oscar.py
+++ borrador_oscar.py
@@ -5,17 +5,16 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 import statsmodels.api as sm
 from statsmodels.stats.weightstats import DescrStatsW
-def filtro1(data,i,region=False, percapita=False):
+def filtro1(data,i,region=False):
+  n=data.shape[0]
   if region:
     n_data=data[data['region']==i]
   else:
     n_data=data[data['provincia']==i]
-  fn_data= n_data.drop(['region','provincia','id','hacina','jcomed','cantmiem'],axis=1)
-  if percapita:
-    fn_data.drop(['gastot','ingtoth'],axis=1,inplace=True)
-  else:
-    fn_data.drop(['gastotpc','ingpch'],axis=1,inplace=True)  
-    #proprauto
+  fn_data= n_data.drop(['region','provincia','id','hacina','jcomed','gastotpc','gastot','ingtoth'],axis=1) 
+
+
+  #gasto bajo es un gasto por debajo de la media de la suma de los primeros 3 grupos de gasto
   g3= (fn_data['gc09_01']+fn_data['gc09_02']+fn_data['gc09_03']).mean()
   g6= (fn_data['gc09_01']+fn_data['gc09_02']+fn_data['gc09_03']+fn_data['gc09_04']+fn_data['gc09_05']+fn_data['gc09_06']).mean()
   g9= (fn_data['gc09_01']+fn_data['gc09_02']+fn_data['gc09_03']+fn_data['gc09_04']+fn_data['gc09_05']+fn_data['gc09_06']+fn_data['gc09_07']+fn_data['gc09_08']+fn_data['gc09_09']).mean()
@@ -24,9 +23,8 @@ def filtro1(data,i,region=False, percapita=False):
   for i in range(l.shape[0]):
     gasto.append(int(l[i][0:2].sum()>g3)+int(l[i][0:5].sum()>g6)+int(l[i][0:8].sum()>g9))
   fn_data['gasto']=gasto
-  
+  #proprauto
   fn_data['propauto'] =  fn_data['propauto'].apply(lambda x: 1 if x in [2, 3] else 0)
-
 
   #regten regimen de tenencia
   fn_data['regten']=  fn_data['regten'].apply(lambda x: 1 if x==1 else 0)
@@ -37,10 +35,6 @@ def filtro1(data,i,region=False, percapita=False):
   fn_data['univ']=  fn_data['jniveled'].apply(lambda x: 1 if x==6 else 0)
   #//////////////////////////////////////////
   fn_data= fn_data.drop(['jniveled'],axis=1)
-
-
-  fn_data['jsexo']= fn_data['jsexo']-1
-
 
 
   fn_data['edad_25/34']= fn_data['jedad_agrup'].apply(lambda x: 1 if x==3 else 0)
@@ -81,114 +75,24 @@ data = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/diplo/engho2018_hogar
 #por ahora sigue siendo el modelo mas simple que agarra la menor cantidad variables posibles*********************************** no olvidar borrar cuando se creen modelos mas complejos
 #filtrado
 #mayor_tamaño_provincia=data['provincia'].value_counts().keys()[index(max(data['provincia'].value_counts().values()))]
-fdata_nea=filtro1(data,4,region=True,percapita=True)
+fdata_nea=filtro1(data,4,region=True)
+n=fdata_nea.shape[0]
 
-#/////////////////////////////////////////////
-
-#calculos/////////////////////////////////////////
-m_propauto= (fdata_nea['propauto']*fdata_nea['pondera']).mean()
-dvp1=DescrStatsW(fdata_nea['propauto'], weights=fdata_nea['pondera'])
-
-
-mreg1= (fdata_nea['propietario']*fdata_nea['pondera']).mean()
-mreg2= (fdata_nea['inquilino']*fdata_nea['pondera']).mean()
-mreg3= (fdata_nea['ocupante']*fdata_nea['pondera']).mean()
-dvp1= DescrStatsW(fdata_nea['propietario'], weights=fdata_nea['pondera'])
-dvp2= DescrStatsW(fdata_nea['inquilino'], weights=fdata_nea['pondera'])
-dvp3= DescrStatsW(fdata_nea['ocupante'], weights=fdata_nea['pondera'])
-
-
-mprim= (fdata_nea['prim']*fdata_nea['pondera']).mean()
-msec= (fdata_nea['sec']*fdata_nea['pondera']).mean()
-muniv= (fdata_nea['univ']*fdata_nea['pondera']).mean()
-dvprim= DescrStatsW(fdata_nea['prim'], weights=fdata_nea['pondera'])
-dvsec= DescrStatsW(fdata_nea['sec'], weights=fdata_nea['pondera'])
-dvuniv= DescrStatsW(fdata_nea['univ'], weights=fdata_nea['pondera'])
-
-
-mjedad_agrup= (fdata_nea['jedad_agrup']*fdata_nea['pondera']).mean()
-dvpjedad_agrup= DescrStatsW(fdata_nea['jedad_agrup'], weights=fdata_nea['pondera'])
-
-
-mcasado= (fdata_nea['casado']*fdata_nea['pondera']).mean()
-dvcasado= DescrStatsW(fdata_nea['casado'], weights=fdata_nea['pondera'])
-
-
-
-mjestado= (fdata_nea['jestado']*fdata_nea['pondera']).mean()
-dvpjestado= DescrStatsW(fdata_nea['jestado'], weights=fdata_nea['pondera'])
-
-
-
-mas= (fdata_nea['as']*fdata_nea['pondera']).mean()
-dvas= DescrStatsW(fdata_nea['as'], weights=fdata_nea['pondera'])
-
-
-
-msoc_jur= (fdata_nea['soc_jur']*fdata_nea['pondera']).mean()
-dvsoc_jur= DescrStatsW(fdata_nea['soc_jur'], weights=fdata_nea['pondera'])
-
-
-
-mjpercept=(fdata_nea['jpercept']*fdata_nea['pondera']).mean()
-dvpjpercept= DescrStatsW(fdata_nea['jpercept'], weights=fdata_nea['pondera'])
-
-
-
-mclima_educativo= (fdata_nea['clima_educativo']*fdata_nea['pondera']).mean()
-dvclima_educativo= DescrStatsW(fdata_nea['clima_educativo'], weights=fdata_nea['pondera'])
-
-
-'''
-corr_datos1 = fdn1[['gastot','ingtoth','clima_educativo','as','jestado','casado','jedad_agrup','univ','prim','regten2','regten1','propauto','cantmiem']].corr()
-corr_datos2 = fdn2[['gastot','ingtoth','clima_educativo','as','jestado','casado','jedad_agrup','univ','prim','regten2','regten1','propauto','menor18','menor14','mayor65']].corr()
-plt.figure(figsize=(8, 6))
-sns.heatmap(corr_datos1, annot=True)
-#sns.heatmap(corr_datos2, annot=True)
-
-plt.show()
-'''
-'''
-
-variables = ['gastot','ingtoth','clima_educativo','as','jestado','casado','jedad_agrup','univ','sec','prim','regten2','regten1','propauto','menor18','menor14','mayor65']
-titulos = ['gastot','ingtoth','clima_educativo','as','jestado','casado','jedad_agrup','univ','sec','prim','regten2','regten1','propauto','menor18','menor14','mayor65']
-xs = ['gastot','ingtoth','clima_educativo','as','jestado','casado','jedad_agrup','univ','sec','prim','regten2','regten1','propauto','menor18','menor14','mayor65']
-ys = ['Frecuencia',None,None,'Frecuencia',None,None, 'Frecuencia',None,None,'Frecuencia',None,None]
-
-fig, ax = plt.subplots(4, 4, figsize=(16,16))
-
-for i in range(4):
-  for j in range(4):
-    ax[i,j].hist(fdn2[variables[i*2+j]], edgecolor = "white")
-    ax[i,j].set_title(titulos[i*2+j])
-    ax[i,j].set_xlabel(xs[i*2+j])
-    ax[i,j].set_ylabel(ys[i*2+j])
-
-plt.subplots_adjust(hspace=0.7)
-plt.show()
-'''
-
-'''#
-X = fdn2['ingtoth']
+X = fdata_nea['ingpch']
 X = sm.add_constant(X)
-y = fdn2[['gastot']]
+y = fdata_nea[['gasto']]
 
 reg1 = sm.OLS(y,X).fit()
 print(reg1.summary())
 
 
-X = fdn2[['ingtoth','clima_educativo','as','jestado','casado','jedad_agrup','univ','sec','prim','regten2','regten1','propauto','menor18','menor14','mayor65']]
+X = fdata_nea[['ingpch','clima_educativo','asalariado','propia_soc_jur','propia_soc_no_jur','socio_soc_jur','socio_soc_no_jur','jestado','casado','univ','sec','prim','regten','propauto','cantmiem','edad_25/34','edad_35/49','edad65_mas']]
 X = sm.add_constant(X)
-y = fdn2[['gastot']]
+y = fdata_nea[['gasto']]
 
 reg2 = sm.OLS(y,X).fit()
 print(reg2.summary())
-'''
-'''
-plt.scatter(['tamanio_motor'], base['emisiones_co2'])
-plt.title('Tamaño del motor vs emisiones CO2')
-plt.xlabel('Litros')
-plt.ylabel('g/km')
+plt.hist(np.log(fdata_nea['gasto'][fdata_nea['gasto']>0]))
 plt.show()
-#print(fdata_nea.describe())
-'''
+plt.hist(np.log(fdata_nea['ingpch'][fdata_nea['ingpch']>0]))
+plt.show()
