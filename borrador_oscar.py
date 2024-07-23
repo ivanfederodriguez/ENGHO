c3a782ee44a73a24d7c18ff1b2a0ac23eb9e8e13
oscarinn295 <oscarmolinas295@gmail.com>
2024-07-22 23:18:41 -0300

    Update borrador_oscar.py

--- borrador_oscar.py
+++ borrador_oscar.py
@@ -16,7 +16,7 @@ def filtro1(data,i,region=False, percapita=False):
   else:
     fn_data.drop(['gastotpc','ingpch'],axis=1,inplace=True)  
     #proprauto
-   g3= fn_data[['gc09_01','gc09_02','gc09_03']].mean(axis=1)
+  g3= fn_data[['gc09_01','gc09_02','gc09_03']].mean(axis=1)
   g6= fn_data[['gc09_01','gc09_02','gc09_03','gc09_04','gc09_05','gc09_06']].mean(axis=1)
   g9= fn_data[['gc09_01','gc09_02','gc09_03','gc09_04','gc09_05','gc09_06','gc09_07','gc09_08','gc09_09']].mean(axis=1)
   fn_data['gasto']=(fn_data[['gc09_01','gc09_02','gc09_03','gc09_04','gc09_05','gc09_06','gc09_07','gc09_08','gc09_09']].apply(lambda x: (x.iloc[0:2].sum()>g3)+(x.iloc[3:5].sum()>g6)+(x.iloc[6:8].sum()>g9),axis=1)).mean(axis=1)
